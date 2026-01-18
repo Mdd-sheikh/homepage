d951a431474dcd2ed007c4069d85ee98349a8df4
Your Name <your-email@example.com>
2026-01-18 03:47:37 -0800

    fix line endings

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Home/>
      <h1>now im starting to development</h1>
      <p>today im gonna to do something</p>
-     
+      
     </>
   )
 }
