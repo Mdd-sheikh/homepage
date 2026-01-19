80c10fe7b5b171516182b3be3e268f824282539c
Your Name <your-email@example.com>
2026-01-19 02:35:17 -0800

    fix line endings

--- home/src/head-foot/Slider/Slider.jsx
+++ home/src/head-foot/Slider/Slider.jsx
@@ -7,18 +7,18 @@ const Slider = () => {
         <div className="slider-container">
            <div className="upper-slider">
              <ul>
-                <li><i class="fa-solid fa-house"></i>Home</li>
-                <li><i class="fa-solid fa-gift"></i>Product</li>
-                <li><i class="fa-brands fa-stack-overflow"></i>Category</li>
-                <li><i class="fa-solid fa-book"></i>Stores</li>
-                <li><i class="fa-solid fa-dollar-sign"></i>Finances</li>
-                <li><i class="fa-solid fa-gear"></i>Setting</li>
+                <li><i class="fa-solid fa-house"></i><span>Home</span></li>
+                <li><i class="fa-solid fa-gift"></i><span>Product</span></li>
+                <li><i class="fa-brands fa-stack-overflow"></i><span>Category</span></li>
+                <li><i class="fa-solid fa-book"></i><span>Stores</span></li>
+                <li><i class="fa-solid fa-dollar-sign"></i><span>Finances</span></li>
+                <li><i class="fa-solid fa-gear"></i><span>Setting</span></li>
             </ul>
            </div>
            <div className="lower-slider">
             <ul>
-                <li><i class="fa-solid fa-plus"></i>Add Product</li>
-                <li><i class="fa-solid fa-arrow-up-from-bracket"></i>Log out</li>
+                <li><i class="fa-solid fa-plus"></i><span>Add Product</span></li>
+                <li><i class="fa-solid fa-arrow-up-from-bracket"></i><span>Log out</span></li>
             </ul>
            </div>
         </div>
